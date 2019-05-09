0ee8bb05c81da2a93706bca5d3dabbdfef7b5cc1
radcatblack <45980138+radcatblack@users.noreply.github.com>
2019-05-09 10:19:19 +0200

    Add files via upload

---
+++ AoC_2018_2.js
@@ -0,0 +1,320 @@
+
+let allBoxes = "evsialkqydurohxqpwbcugtjmh\n" +
+    "evsialkqydurohxzssbcngtjmv\n" +
+    "fvlialkqydurohxzpwbcngujmf\n" +
+    "nvsialkqydorohxzpwpcngtjmf\n" +
+    "evsialjqydnrohxypwbcngtjmf\n" +
+    "vvsialyqxdurohxzpwbcngtjmf\n" +
+    "yvsialksydurowxzpwbcngtjmf\n" +
+    "evsillkqydurbhxzpmbcngtjmf\n" +
+    "ivsialkqyxurshxzpwbcngtjmf\n" +
+    "ejsiagkqyduhohxzpwbcngtjmf\n" +
+    "evsialkqldurohxzpcbcngtjmi\n" +
+    "evsialkqydurohxzpsbyngtkmf\n" +
+    "ersialkeydurohxzpwbcngtpmf\n" +
+    "evsialuqzdkrohxzpwbcngtjmf\n" +
+    "evswulkpydurohxzpwbcngtjmf\n" +
+    "evsialkqyiurohxzpwucngttmf\n" +
+    "evtialkqydurphxzywbcngtjmf\n" +
+    "evsialkzyiurohxzpwbcxgtjmf\n" +
+    "evsiaykqydurohxzpwbcggtjuf\n" +
+    "evxqalkqydurohmzpwbcngtjmf\n" +
+    "eisralkqydurohxzpdbcngtjmf\n" +
+    "evsfalkqydurohxzpwbangtjwf\n" +
+    "evbialkqydurohxzawbcngtjmg\n" +
+    "evsialkqydrrohxrpcbcngtjmf\n" +
+    "evsialkqycurohxzpvbcngtjkf\n" +
+    "evsialkqsdudohxzpwbcnotjmf\n" +
+    "evsiackqydurohxzpmbsngtjmf\n" +
+    "evsialmqykurohxzpwbfngtjmf\n" +
+    "evsialsqydurohxzpwucngtjxf\n" +
+    "tvsialkqyeurohxzpwbcrgtjmf\n" +
+    "zvsialkqydbrohxzpwbcnltjmf\n" +
+    "evsmbskqydurohxzpwbcngtjmf\n" +
+    "evsialkqydurohxzpwbcngpgmt\n" +
+    "evsialkqydurlyezpwbcngtjmf\n" +
+    "evoialkqyturohxzpwbcnjtjmf\n" +
+    "evsialkqydurohxspkfcngtjmf\n" +
+    "evsiaikqydurohxjpwbcngtjmd\n" +
+    "evsialkyydurohxzvwbcngtjmc\n" +
+    "svsialkqyduhohxzpwbhngtjmf\n" +
+    "eysillkqydurohxzhwbcngtjmf\n" +
+    "evsialkqyduetaxzpwbcngtjmf\n" +
+    "evsialkqxdurshxzpwbcngtjmb\n" +
+    "evsiadkqydwrovxzpwbcngtjmf\n" +
+    "evsialkqydurokxzpwbcngjjef\n" +
+    "evskalkqymurohxzpybcngtjmf\n" +
+    "cvsialkqydurohxzpwbcnbtjma\n" +
+    "evsialkqydurohxzawhcngtjuf\n" +
+    "evsiahkqfduroixzpwbcngtjmf\n" +
+    "evsivlkqyduroqxzpwbctgtjmf\n" +
+    "evsiarkqyduroixzywbcngtjmf\n" +
+    "evspalkqydurohxzpwlcngxjmf\n" +
+    "eesialkqydurohxzpalcngtjmf\n" +
+    "gvsualkqydurohxzpwbmngtjmf\n" +
+    "evsialkqydurlhxzpwbcngsjmq\n" +
+    "evsialhqydfrohxopwbcngtjmf\n" +
+    "evzialkqydsrohxzpwbcngtjmw\n" +
+    "evbpalkqydurbhxzpwbcngtjmf\n" +
+    "mvsialkqydurohxzpwbcnghjmr\n" +
+    "evsialkqsdurohxzpkbcngtjxf\n" +
+    "ejkialktydurohxzpwbcngtjmf\n" +
+    "evsialkqyauoohxzpwbqngtjmf\n" +
+    "evsiklkyyduroqxzpwbcngtjmf\n" +
+    "evgialkqydurohxzpwocngthmf\n" +
+    "ebsialkqydcrohxzpwbcngtbmf\n" +
+    "evsialkqysurohxzpwfingtjmf\n" +
+    "evsialkqddurmhxzpwbnngtjmf\n" +
+    "evsialkqydurohxoiwwcngtjmf\n" +
+    "evsialkqydurohpzkzbcngtjmf\n" +
+    "vvsealkqydurorxzpwbcngtjmf\n" +
+    "evsialkqyduroqxzpwlungtjmf\n" +
+    "eviialkqiyurohxzpwbcngtjmf\n" +
+    "evzsalkqyaurohxzpwbcngtjmf\n" +
+    "exsialkqydurohfzpwbwngtjmf\n" +
+    "evsialkqyduruhxkpwbcnytjmf\n" +
+    "essiatkqydurohxzpwbxngtjmf\n" +
+    "evsialkqyduroamzpwbcngtjcf\n" +
+    "wvsialkqyduruhxzpwbcnxtjmf\n" +
+    "evsialkqydurohxgpwbcngtjeh\n" +
+    "evsialfqxdurohxzpwbcngtomf\n" +
+    "evsialkqyourghxzpwbcngtbmf\n" +
+    "evsoaokqydurohxzpwbcngtamf\n" +
+    "evsialpqydurohxzpwccxgtjmf\n" +
+    "evsialkqzdurxhxgpwbcngtjmf\n" +
+    "ezsialkqmdurohxzpwbcngtjmi\n" +
+    "cvsialjeydurohxzpwbcngtjmf\n" +
+    "evsialkqydurocxupwbcvgtjmf\n" +
+    "evscalkqydtrohxzpebcngtjmf\n" +
+    "evjialkqyduiohxzpabcngtjmf\n" +
+    "evsialjqyduruhxzppbcngtjmf\n" +
+    "evsialkqydurfhxzpwbcuqtjmf\n" +
+    "evsialkqyiurohizpwucngttmf\n" +
+    "evsialiqydurrhxzpwbcngdjmf\n" +
+    "evbialkqywurohxzpwhcngtjmf\n" +
+    "evsialkqyduloyxzpwbqngtjmf\n" +
+    "evsialxqyduzohxzpwbqngtjmf\n" +
+    "vvsialkqydurohxzpwbcnqpjmf\n" +
+    "evsialksydurohxzcwbmngtjmf\n" +
+    "pvsialkqydurohxzpwucngtjvf\n" +
+    "evsialkqydurohmkpwbcngtfmf\n" +
+    "mvsialkqydurphyzpwbcngtjmf\n" +
+    "evsialkqydyrohxzhwbcnitjmf\n" +
+    "evsialokydurozxzpwbcngtjmf\n" +
+    "evsialkqyduroexfcwbcngtjmf\n" +
+    "evsiavkqydurohxzpwbcnmtjme\n" +
+    "evsiawkqydurohxzpwbcngojjf\n" +
+    "evsialkaydurohxzpwfcngtjff\n" +
+    "evsialkaydurohxzpwbcngtjpb\n" +
+    "gvsialkqyburorxzpwbcngtjmf\n" +
+    "evszalkqydurphxzpwocngtjmf\n" +
+    "evsualkqyduropxzpwbcngejmf\n" +
+    "evsitlkqydurshxzpwbcngtkmf\n" +
+    "evbixlkqydrrohxzpwbcngtjmf\n" +
+    "elsialkqydprohxzpwbcngtrmf\n" +
+    "evsialkqydurohbzpwbcggtjmc\n" +
+    "evtoalqqydurohxzpwbcngtjmf\n" +
+    "evsralhqydurohxzowbcngtjmf\n" +
+    "evsialkhydurohxzlsbcngtjmf\n" +
+    "evsialkqydurohxvpwbcnuujmf\n" +
+    "evsialkqydurocxzuwbcngtjmi\n" +
+    "evsialkqndyrokxzpwbcngtjmf\n" +
+    "evsialkqydurywfzpwbcngtjmf\n" +
+    "evsialkqydurohxzwwbcngthms\n" +
+    "eqsiahkqydurohxzpwbyngtjmf\n" +
+    "evsdalkqydurohxzpwbcnjkjmf\n" +
+    "evsialkqyddrohplpwbcngtjmf\n" +
+    "evshalkqydurohxzpfxcngtjmf\n" +
+    "evvialkqydurohxapwbcngtjmh\n" +
+    "evsialkqyduvohxzpwbcnnvjmf\n" +
+    "evsiblkqedurohxzpwbkngtjmf\n" +
+    "evsvalkqfdutohxzpwbcngtjmf\n" +
+    "evsialjqydurohxzpwbcnctjsf\n" +
+    "evsialkxywurohxdpwbcngtjmf\n" +
+    "evsiagkqydurohxzpwzcjgtjmf\n" +
+    "ebsialkqydurohxzpxfcngtjmf\n" +
+    "evsialkqysfrohxzpwbcngtjlf\n" +
+    "evvialkqyqurwhxzpwbcngtjmf\n" +
+    "evxialkqydurohxzpwgcnrtjmf\n" +
+    "vvsillkqydurohxzpwbcvgtjmf\n" +
+    "evsiwlkqyduoohxzpwbcngtjxf\n" +
+    "evsialkqypurohezpwbcngtjwf\n" +
+    "evbialkqydurohxipwbcnftjmf\n" +
+    "evsiakkqyduyohxzpwbcngtjmu\n" +
+    "evsialkqydurohzzpwxqngtjmf\n" +
+    "evsialkqykurkhxzpwocngtjmf\n" +
+    "dvriplkqydurohxzpwbcngtjmf\n" +
+    "evsialkqgdurohxzpwbmnctjmf\n" +
+    "evsialkqyuurohxzpwtcngtjmj\n" +
+    "wvsialkqydurohxzpwbchgejmf\n" +
+    "eusimlsqydurohxzpwbcngtjmf\n" +
+    "evsialkqydqrohxzhwbcngtjmh\n" +
+    "wvswalkqydurohxzpwbcngjjmf\n" +
+    "evsialkqyourohxzkwbcngttmf\n" +
+    "evaialkqydurohxzbubcngtjmf\n" +
+    "evfialkqydueohxzpwbclgtjmf\n" +
+    "evrialkqydurohxzpwbcnctjmh\n" +
+    "evsiaojqydxrohxzpwbcngtjmf\n" +
+    "evsualkqywuxohxzpwbcngtjmf\n" +
+    "evsialkdydrzohxzpwbcngtjmf\n" +
+    "evlialkqyfurohxzpwbcnotjmf\n" +
+    "epsialkqydujohxzpwbcngtjif\n" +
+    "evsialkqyaucohxgpwbcngtjmf\n" +
+    "lvsialaqydurohxzpwbcngtjzf\n" +
+    "evsialkgydurohezpwbcngtjmo\n" +
+    "lvsialkqydurosxwpwbcngtjmf\n" +
+    "evsiaekqyqurohxzpvbcngtjmf\n" +
+    "evsiapkqydirohxzpwbzngtjmf\n" +
+    "zvsixlkwydurohxzpwbcngtjmf\n" +
+    "evaialkqyduoohxzpwbcngtjkf\n" +
+    "evsialcqedurohxzpwbcngtjmc\n" +
+    "evjialkgydurohxzpwbwngtjmf\n" +
+    "evsialkqcdurohxzpwbcpgojmf\n" +
+    "evsialkqkdurohxzlwbcngtrmf\n" +
+    "eosiylkzydurohxzpwbcngtjmf\n" +
+    "evsialkqydurohhzpwscnmtjmf\n" +
+    "evsiallqydurobxzpwbxngtjmf\n" +
+    "evsialkqydurohwztwhcngtjmf\n" +
+    "evsiallqydurohxzpwbcygjjmf\n" +
+    "evsiabkqywurohxzpwbcngtjmy\n" +
+    "evsiackqydzrohxznwbcngtjmf\n" +
+    "evsiazkqzdurooxzpwbcngtjmf\n" +
+    "evsialcqydurghxzpwbcngtjmc\n" +
+    "yvsiaxkqydurohxzpwbcxgtjmf\n" +
+    "evsiylkqgdhrohxzpwbcngtjmf\n" +
+    "lvsialkqydurohxgcwbcngtjmf\n" +
+    "evsiglkqydurohxzpwbvngzjmf\n" +
+    "evsialkqyvurohxzpwbcngtjnz\n" +
+    "evsialkgydueohxzpwbcpgtjmf\n" +
+    "cvsiavkqyddrohxzpwbcngtjmf\n" +
+    "evsialklyrurohxzpwbcngtjff\n" +
+    "eisialkqyduwohxzpwbcngcjmf\n" +
+    "evsialkqydrrihwzpwbcngtjmf\n" +
+    "easialkqydurohxzpwbcnltrmf\n" +
+    "evsialfqydurohxzpybcnytjmf\n" +
+    "eqsialkqycurohxzywbcngtjmf\n" +
+    "evsitlkqmdurohxzpwbcngtjmx\n" +
+    "evsiclsqyduroixzpwbcngtjmf\n" +
+    "elsialrqydurohxzpwmcngtjmf\n" +
+    "evsiapkqodurohxzpwbcogtjmf\n" +
+    "evstalkeydurohxzpibcngtjmf\n" +
+    "evsihlkqyqurohxzpwblngtjmf\n" +
+    "euszalkqydurohxipwbcngtjmf\n" +
+    "ezsialksydurohxzpwbcngfjmf\n" +
+    "eisialkdydurohxzpwbcngtumf\n" +
+    "evsirlkaydprohxzpwbcngtjmf\n" +
+    "evsiklkqydnrohxzpwbcngtjmu\n" +
+    "evsialkqydnuohxzpwbcngtjmu\n" +
+    "eksialkqydurohxztwfcngtjmf\n" +
+    "evlialkqedurohxzpwbhngtjmf\n" +
+    "evqialkqydurohxzpubcngtjpf\n" +
+    "evsialkwydurohwzpwbcnmtjmf\n" +
+    "evsiaokqcdurohxzpwbcngtjcf\n" +
+    "evsialkkyfurohxzpvbcngtjmf\n" +
+    "evsialkqyduromxzpwqcngtimf\n" +
+    "evsialkqydumohxzpwbcnmtjsf\n" +
+    "evsialddydurehxzpwbcngtjmf\n" +
+    "evsialkqydurohxzpobcnptjmk\n" +
+    "evsiagkqydurohhzpwbcxgtjmf\n" +
+    "evsfalkqydurohszpwbangtjmf\n" +
+    "evgialkzyduqohxzpwbcngtjmf\n" +
+    "evaialkqzdurohxzpwbcngtjmo\n" +
+    "evsialkqyqurohxjpwbcnntjmf\n" +
+    "evsialkjydybohxzpwbcngtjmf\n" +
+    "evskalgqydurohxzrwbcngtjmf\n" +
+    "evsialkqydurohxzpjbcymtjmf\n" +
+    "evsialkqqdurohxzpybcngtjyf\n" +
+    "evsialkqydqrbhxzpwbcngtjmj\n" +
+    "evssalaqrdurohxzpwbcngtjmf\n" +
+    "mvsialkfydurohxzpwbcngtjmk\n" +
+    "evsialkqwdurohxzpwgcngtjdf\n" +
+    "evqkalkqydurohxzpwbcngajmf\n" +
+    "evbialkqydurohxzpibcngejmf\n" +
+    "evszalkqydurbhxzpwbcngtjsf\n" +
+    "evsialkqydurohxepwbcngtjjo\n" +
+    "evsialkqcdubmhxzpwbcngtjmf\n" +
+    "evsiarkqyduroaxzpwbcngtjmp\n" +
+    "evsiakkqyduzohczpwbcngtjmf\n" +
+    "evtualkqydurofxzpwbcngtjmf\n" +
+    "ejsialkqvdurohzzpwbcngtjmf\n" +
+    "evsialkqydurohczpwbcngqvmf\n" +
+    "svsianfqydurohxzpwbcngtjmf\n" +
+    "evsialiqydurohxzpwbcngzqmf\n" +
+    "ejsialhqydurohxzpwjcngtjmf\n" +
+    "evpialkqydurohxzpwbcnbtjff\n" +
+    "evsialkuyvurohxzpwbcngtjkf\n" +
+    "eqsialkqydurohxzpwbcnwtcmf\n" +
+    "evsiatkqydkrohxzpwkcngtjmf\n" +
+    "evsialkqydurohxzpebciytjmf\n" +
+    "evsialkqydrrohxzpwtcngtfmf\n" +
+    "evsialkqjducohxzpwycngtjmf\n" +
+    "evsialkqydurohxzpwicnxtjnf";
+
+
+
+let checksum = 0;
+let splitBoxList = allBoxes.split("\n");
+let BoxIndex = 0;
+let TwoLetterBoxes = 0;
+let ThreeLetterBoxes = 0;
+
+
+while (BoxIndex < splitBoxList.length) {
+
+    let currentBox = splitBoxList[BoxIndex];
+    let letters = currentBox.split("");
+    let letterIndex = 0;
+    let IsTwoLetter = false;
+    let IsThreeLetter = false;
+
+    while (letterIndex < letters.length) {
+        let currentLetter = letters[letterIndex];
+        let checkLetterIndex = 0;
+        let count = 0;
+        letterIndex = letterIndex + 1;
+
+        while (checkLetterIndex < letters.length) {
+            let checkLetter = letters[checkLetterIndex];
+
+            if (currentLetter == checkLetter) {
+                count = count + 1;
+            }
+
+            checkLetterIndex = checkLetterIndex + 1;
+
+            //somewhere save result as well
+        }
+
+
+        if (count == 2) {
+            IsTwoLetter = true;
+        }
+
+        if (count == 3) {
+            IsThreeLetter = true;
+        }
+
+
+    }
+
+   if (IsTwoLetter == true) {
+       TwoLetterBoxes = TwoLetterBoxes + 1;
+   }
+
+   if (IsThreeLetter == true) {
+       ThreeLetterBoxes = ThreeLetterBoxes + 1;
+   }
+
+    BoxIndex = BoxIndex + 1;
+}
+console.log("TwoLetterBoxes", TwoLetterBoxes);
+console.log("Threeletterboxes", ThreeLetterBoxes);
+
+checksum = TwoLetterBoxes * ThreeLetterBoxes;
+console.log("checksum", checksum);
+
+
+//how many "boxes" have a letter repeating twice
+
+//how many boxes have a letter repeating thrice
+
---
+++ AoC_2018_2_2.js
@@ -0,0 +1,350 @@
+let allBoxes = "evsialkqydurohxqpwbcugtjmh\n" +
+    "evsialkqydurohxzssbcngtjmv\n" +
+    "fvlialkqydurohxzpwbcngujmf\n" +
+    "nvsialkqydorohxzpwpcngtjmf\n" +
+    "evsialjqydnrohxypwbcngtjmf\n" +
+    "vvsialyqxdurohxzpwbcngtjmf\n" +
+    "yvsialksydurowxzpwbcngtjmf\n" +
+    "evsillkqydurbhxzpmbcngtjmf\n" +
+    "ivsialkqyxurshxzpwbcngtjmf\n" +
+    "ejsiagkqyduhohxzpwbcngtjmf\n" +
+    "evsialkqldurohxzpcbcngtjmi\n" +
+    "evsialkqydurohxzpsbyngtkmf\n" +
+    "ersialkeydurohxzpwbcngtpmf\n" +
+    "evsialuqzdkrohxzpwbcngtjmf\n" +
+    "evswulkpydurohxzpwbcngtjmf\n" +
+    "evsialkqyiurohxzpwucngttmf\n" +
+    "evtialkqydurphxzywbcngtjmf\n" +
+    "evsialkzyiurohxzpwbcxgtjmf\n" +
+    "evsiaykqydurohxzpwbcggtjuf\n" +
+    "evxqalkqydurohmzpwbcngtjmf\n" +
+    "eisralkqydurohxzpdbcngtjmf\n" +
+    "evsfalkqydurohxzpwbangtjwf\n" +
+    "evbialkqydurohxzawbcngtjmg\n" +
+    "evsialkqydrrohxrpcbcngtjmf\n" +
+    "evsialkqycurohxzpvbcngtjkf\n" +
+    "evsialkqsdudohxzpwbcnotjmf\n" +
+    "evsiackqydurohxzpmbsngtjmf\n" +
+    "evsialmqykurohxzpwbfngtjmf\n" +
+    "evsialsqydurohxzpwucngtjxf\n" +
+    "tvsialkqyeurohxzpwbcrgtjmf\n" +
+    "zvsialkqydbrohxzpwbcnltjmf\n" +
+    "evsmbskqydurohxzpwbcngtjmf\n" +
+    "evsialkqydurohxzpwbcngpgmt\n" +
+    "evsialkqydurlyezpwbcngtjmf\n" +
+    "evoialkqyturohxzpwbcnjtjmf\n" +
+    "evsialkqydurohxspkfcngtjmf\n" +
+    "evsiaikqydurohxjpwbcngtjmd\n" +
+    "evsialkyydurohxzvwbcngtjmc\n" +
+    "svsialkqyduhohxzpwbhngtjmf\n" +
+    "eysillkqydurohxzhwbcngtjmf\n" +
+    "evsialkqyduetaxzpwbcngtjmf\n" +
+    "evsialkqxdurshxzpwbcngtjmb\n" +
+    "evsiadkqydwrovxzpwbcngtjmf\n" +
+    "evsialkqydurokxzpwbcngjjef\n" +
+    "evskalkqymurohxzpybcngtjmf\n" +
+    "cvsialkqydurohxzpwbcnbtjma\n" +
+    "evsialkqydurohxzawhcngtjuf\n" +
+    "evsiahkqfduroixzpwbcngtjmf\n" +
+    "evsivlkqyduroqxzpwbctgtjmf\n" +
+    "evsiarkqyduroixzywbcngtjmf\n" +
+    "evspalkqydurohxzpwlcngxjmf\n" +
+    "eesialkqydurohxzpalcngtjmf\n" +
+    "gvsualkqydurohxzpwbmngtjmf\n" +
+    "evsialkqydurlhxzpwbcngsjmq\n" +
+    "evsialhqydfrohxopwbcngtjmf\n" +
+    "evzialkqydsrohxzpwbcngtjmw\n" +
+    "evbpalkqydurbhxzpwbcngtjmf\n" +
+    "mvsialkqydurohxzpwbcnghjmr\n" +
+    "evsialkqsdurohxzpkbcngtjxf\n" +
+    "ejkialktydurohxzpwbcngtjmf\n" +
+    "evsialkqyauoohxzpwbqngtjmf\n" +
+    "evsiklkyyduroqxzpwbcngtjmf\n" +
+    "evgialkqydurohxzpwocngthmf\n" +
+    "ebsialkqydcrohxzpwbcngtbmf\n" +
+    "evsialkqysurohxzpwfingtjmf\n" +
+    "evsialkqddurmhxzpwbnngtjmf\n" +
+    "evsialkqydurohxoiwwcngtjmf\n" +
+    "evsialkqydurohpzkzbcngtjmf\n" +
+    "vvsealkqydurorxzpwbcngtjmf\n" +
+    "evsialkqyduroqxzpwlungtjmf\n" +
+    "eviialkqiyurohxzpwbcngtjmf\n" +
+    "evzsalkqyaurohxzpwbcngtjmf\n" +
+    "exsialkqydurohfzpwbwngtjmf\n" +
+    "evsialkqyduruhxkpwbcnytjmf\n" +
+    "essiatkqydurohxzpwbxngtjmf\n" +
+    "evsialkqyduroamzpwbcngtjcf\n" +
+    "wvsialkqyduruhxzpwbcnxtjmf\n" +
+    "evsialkqydurohxgpwbcngtjeh\n" +
+    "evsialfqxdurohxzpwbcngtomf\n" +
+    "evsialkqyourghxzpwbcngtbmf\n" +
+    "evsoaokqydurohxzpwbcngtamf\n" +
+    "evsialpqydurohxzpwccxgtjmf\n" +
+    "evsialkqzdurxhxgpwbcngtjmf\n" +
+    "ezsialkqmdurohxzpwbcngtjmi\n" +
+    "cvsialjeydurohxzpwbcngtjmf\n" +
+    "evsialkqydurocxupwbcvgtjmf\n" +
+    "evscalkqydtrohxzpebcngtjmf\n" +
+    "evjialkqyduiohxzpabcngtjmf\n" +
+    "evsialjqyduruhxzppbcngtjmf\n" +
+    "evsialkqydurfhxzpwbcuqtjmf\n" +
+    "evsialkqyiurohizpwucngttmf\n" +
+    "evsialiqydurrhxzpwbcngdjmf\n" +
+    "evbialkqywurohxzpwhcngtjmf\n" +
+    "evsialkqyduloyxzpwbqngtjmf\n" +
+    "evsialxqyduzohxzpwbqngtjmf\n" +
+    "vvsialkqydurohxzpwbcnqpjmf\n" +
+    "evsialksydurohxzcwbmngtjmf\n" +
+    "pvsialkqydurohxzpwucngtjvf\n" +
+    "evsialkqydurohmkpwbcngtfmf\n" +
+    "mvsialkqydurphyzpwbcngtjmf\n" +
+    "evsialkqydyrohxzhwbcnitjmf\n" +
+    "evsialokydurozxzpwbcngtjmf\n" +
+    "evsialkqyduroexfcwbcngtjmf\n" +
+    "evsiavkqydurohxzpwbcnmtjme\n" +
+    "evsiawkqydurohxzpwbcngojjf\n" +
+    "evsialkaydurohxzpwfcngtjff\n" +
+    "evsialkaydurohxzpwbcngtjpb\n" +
+    "gvsialkqyburorxzpwbcngtjmf\n" +
+    "evszalkqydurphxzpwocngtjmf\n" +
+    "evsualkqyduropxzpwbcngejmf\n" +
+    "evsitlkqydurshxzpwbcngtkmf\n" +
+    "evbixlkqydrrohxzpwbcngtjmf\n" +
+    "elsialkqydprohxzpwbcngtrmf\n" +
+    "evsialkqydurohbzpwbcggtjmc\n" +
+    "evtoalqqydurohxzpwbcngtjmf\n" +
+    "evsralhqydurohxzowbcngtjmf\n" +
+    "evsialkhydurohxzlsbcngtjmf\n" +
+    "evsialkqydurohxvpwbcnuujmf\n" +
+    "evsialkqydurocxzuwbcngtjmi\n" +
+    "evsialkqndyrokxzpwbcngtjmf\n" +
+    "evsialkqydurywfzpwbcngtjmf\n" +
+    "evsialkqydurohxzwwbcngthms\n" +
+    "eqsiahkqydurohxzpwbyngtjmf\n" +
+    "evsdalkqydurohxzpwbcnjkjmf\n" +
+    "evsialkqyddrohplpwbcngtjmf\n" +
+    "evshalkqydurohxzpfxcngtjmf\n" +
+    "evvialkqydurohxapwbcngtjmh\n" +
+    "evsialkqyduvohxzpwbcnnvjmf\n" +
+    "evsiblkqedurohxzpwbkngtjmf\n" +
+    "evsvalkqfdutohxzpwbcngtjmf\n" +
+    "evsialjqydurohxzpwbcnctjsf\n" +
+    "evsialkxywurohxdpwbcngtjmf\n" +
+    "evsiagkqydurohxzpwzcjgtjmf\n" +
+    "ebsialkqydurohxzpxfcngtjmf\n" +
+    "evsialkqysfrohxzpwbcngtjlf\n" +
+    "evvialkqyqurwhxzpwbcngtjmf\n" +
+    "evxialkqydurohxzpwgcnrtjmf\n" +
+    "vvsillkqydurohxzpwbcvgtjmf\n" +
+    "evsiwlkqyduoohxzpwbcngtjxf\n" +
+    "evsialkqypurohezpwbcngtjwf\n" +
+    "evbialkqydurohxipwbcnftjmf\n" +
+    "evsiakkqyduyohxzpwbcngtjmu\n" +
+    "evsialkqydurohzzpwxqngtjmf\n" +
+    "evsialkqykurkhxzpwocngtjmf\n" +
+    "dvriplkqydurohxzpwbcngtjmf\n" +
+    "evsialkqgdurohxzpwbmnctjmf\n" +
+    "evsialkqyuurohxzpwtcngtjmj\n" +
+    "wvsialkqydurohxzpwbchgejmf\n" +
+    "eusimlsqydurohxzpwbcngtjmf\n" +
+    "evsialkqydqrohxzhwbcngtjmh\n" +
+    "wvswalkqydurohxzpwbcngjjmf\n" +
+    "evsialkqyourohxzkwbcngttmf\n" +
+    "evaialkqydurohxzbubcngtjmf\n" +
+    "evfialkqydueohxzpwbclgtjmf\n" +
+    "evrialkqydurohxzpwbcnctjmh\n" +
+    "evsiaojqydxrohxzpwbcngtjmf\n" +
+    "evsualkqywuxohxzpwbcngtjmf\n" +
+    "evsialkdydrzohxzpwbcngtjmf\n" +
+    "evlialkqyfurohxzpwbcnotjmf\n" +
+    "epsialkqydujohxzpwbcngtjif\n" +
+    "evsialkqyaucohxgpwbcngtjmf\n" +
+    "lvsialaqydurohxzpwbcngtjzf\n" +
+    "evsialkgydurohezpwbcngtjmo\n" +
+    "lvsialkqydurosxwpwbcngtjmf\n" +
+    "evsiaekqyqurohxzpvbcngtjmf\n" +
+    "evsiapkqydirohxzpwbzngtjmf\n" +
+    "zvsixlkwydurohxzpwbcngtjmf\n" +
+    "evaialkqyduoohxzpwbcngtjkf\n" +
+    "evsialcqedurohxzpwbcngtjmc\n" +
+    "evjialkgydurohxzpwbwngtjmf\n" +
+    "evsialkqcdurohxzpwbcpgojmf\n" +
+    "evsialkqkdurohxzlwbcngtrmf\n" +
+    "eosiylkzydurohxzpwbcngtjmf\n" +
+    "evsialkqydurohhzpwscnmtjmf\n" +
+    "evsiallqydurobxzpwbxngtjmf\n" +
+    "evsialkqydurohwztwhcngtjmf\n" +
+    "evsiallqydurohxzpwbcygjjmf\n" +
+    "evsiabkqywurohxzpwbcngtjmy\n" +
+    "evsiackqydzrohxznwbcngtjmf\n" +
+    "evsiazkqzdurooxzpwbcngtjmf\n" +
+    "evsialcqydurghxzpwbcngtjmc\n" +
+    "yvsiaxkqydurohxzpwbcxgtjmf\n" +
+    "evsiylkqgdhrohxzpwbcngtjmf\n" +
+    "lvsialkqydurohxgcwbcngtjmf\n" +
+    "evsiglkqydurohxzpwbvngzjmf\n" +
+    "evsialkqyvurohxzpwbcngtjnz\n" +
+    "evsialkgydueohxzpwbcpgtjmf\n" +
+    "cvsiavkqyddrohxzpwbcngtjmf\n" +
+    "evsialklyrurohxzpwbcngtjff\n" +
+    "eisialkqyduwohxzpwbcngcjmf\n" +
+    "evsialkqydrrihwzpwbcngtjmf\n" +
+    "easialkqydurohxzpwbcnltrmf\n" +
+    "evsialfqydurohxzpybcnytjmf\n" +
+    "eqsialkqycurohxzywbcngtjmf\n" +
+    "evsitlkqmdurohxzpwbcngtjmx\n" +
+    "evsiclsqyduroixzpwbcngtjmf\n" +
+    "elsialrqydurohxzpwmcngtjmf\n" +
+    "evsiapkqodurohxzpwbcogtjmf\n" +
+    "evstalkeydurohxzpibcngtjmf\n" +
+    "evsihlkqyqurohxzpwblngtjmf\n" +
+    "euszalkqydurohxipwbcngtjmf\n" +
+    "ezsialksydurohxzpwbcngfjmf\n" +
+    "eisialkdydurohxzpwbcngtumf\n" +
+    "evsirlkaydprohxzpwbcngtjmf\n" +
+    "evsiklkqydnrohxzpwbcngtjmu\n" +
+    "evsialkqydnuohxzpwbcngtjmu\n" +
+    "eksialkqydurohxztwfcngtjmf\n" +
+    "evlialkqedurohxzpwbhngtjmf\n" +
+    "evqialkqydurohxzpubcngtjpf\n" +
+    "evsialkwydurohwzpwbcnmtjmf\n" +
+    "evsiaokqcdurohxzpwbcngtjcf\n" +
+    "evsialkkyfurohxzpvbcngtjmf\n" +
+    "evsialkqyduromxzpwqcngtimf\n" +
+    "evsialkqydumohxzpwbcnmtjsf\n" +
+    "evsialddydurehxzpwbcngtjmf\n" +
+    "evsialkqydurohxzpobcnptjmk\n" +
+    "evsiagkqydurohhzpwbcxgtjmf\n" +
+    "evsfalkqydurohszpwbangtjmf\n" +
+    "evgialkzyduqohxzpwbcngtjmf\n" +
+    "evaialkqzdurohxzpwbcngtjmo\n" +
+    "evsialkqyqurohxjpwbcnntjmf\n" +
+    "evsialkjydybohxzpwbcngtjmf\n" +
+    "evskalgqydurohxzrwbcngtjmf\n" +
+    "evsialkqydurohxzpjbcymtjmf\n" +
+    "evsialkqqdurohxzpybcngtjyf\n" +
+    "evsialkqydqrbhxzpwbcngtjmj\n" +
+    "evssalaqrdurohxzpwbcngtjmf\n" +
+    "mvsialkfydurohxzpwbcngtjmk\n" +
+    "evsialkqwdurohxzpwgcngtjdf\n" +
+    "evqkalkqydurohxzpwbcngajmf\n" +
+    "evbialkqydurohxzpibcngejmf\n" +
+    "evszalkqydurbhxzpwbcngtjsf\n" +
+    "evsialkqydurohxepwbcngtjjo\n" +
+    "evsialkqcdubmhxzpwbcngtjmf\n" +
+    "evsiarkqyduroaxzpwbcngtjmp\n" +
+    "evsiakkqyduzohczpwbcngtjmf\n" +
+    "evtualkqydurofxzpwbcngtjmf\n" +
+    "ejsialkqvdurohzzpwbcngtjmf\n" +
+    "evsialkqydurohczpwbcngqvmf\n" +
+    "svsianfqydurohxzpwbcngtjmf\n" +
+    "evsialiqydurohxzpwbcngzqmf\n" +
+    "ejsialhqydurohxzpwjcngtjmf\n" +
+    "evpialkqydurohxzpwbcnbtjff\n" +
+    "evsialkuyvurohxzpwbcngtjkf\n" +
+    "eqsialkqydurohxzpwbcnwtcmf\n" +
+    "evsiatkqydkrohxzpwkcngtjmf\n" +
+    "evsialkqydurohxzpebciytjmf\n" +
+    "evsialkqydrrohxzpwtcngtfmf\n" +
+    "evsialkqjducohxzpwycngtjmf\n" +
+    "evsialkqydurohxzpwicnxtjnf";
+
+/*function (a) {
+    if (currentCharacter == compareCharacter) {
+        truthDeterminator = truthDeterminator;
+    }
+
+    if (currentCharacter != compareCharacter) {
+        truthDeterminator = truthDeterminator +1;
+    }
+}
+*/
+
+function AreBoxesDifferentByOne (BoxA, BoxB) {
+    let index = 0;
+    let count = 0;
+    let Answer = [];
+    let NoAnswer = [];
+
+    while (index < BoxA.length) {
+        let currentCharacter = BoxA.charAt(index);
+        let compareCharacter = BoxB.charAt(index);
+
+        if (currentCharacter == compareCharacter) {
+            index = index +1;
+            Answer.push (currentCharacter);
+        }
+
+        if (currentCharacter != compareCharacter) {
+            count = count +1;
+            index = index +1;
+        }
+    }
+
+    if (count == 1) {
+        return Answer;
+    }
+
+    if (count != 1) {
+        return NoAnswer;
+    }
+
+}
+
+
+
+
+
+/*function AreBoxesDifferentByOne - test
+
+let TestBox1 = "abdff";
+let TestBox2 = "acdef";
+
+let ergebnis = AreBoxesDifferentByOne (TestBox1, TestBox2);
+console.log(ergebnis);
+*/
+
+
+
+
+
+let singleBoxes = allBoxes.split("\n");
+let BoxIndex = 0;
+let fertig = false;
+
+while (BoxIndex < singleBoxes.length){
+    let currentBox = singleBoxes[BoxIndex];
+    let CompareBoxIndex = 0;
+
+    while (CompareBoxIndex < singleBoxes.length){
+        let CompareBox = singleBoxes[CompareBoxIndex];
+
+        let ergebnis = AreBoxesDifferentByOne (currentBox, CompareBox);
+
+        if (ergebnis.length > 0) {
+            console.log(currentBox, "\n" + CompareBox);
+            let print = ergebnis.join ("");
+            console.log(print);
+            fertig = true;
+            break;
+        }
+
+        CompareBoxIndex = CompareBoxIndex +1;
+    }
+
+    if (fertig == true) {
+        break;
+    }
+
+    BoxIndex = BoxIndex +1;
+}
+
+
+
+//compare all boxes
+//give all letters on a box a position
+// compare positions - if the same => true and move on in box position no.
+// if different => wrong and ad 1 to BoxDetermination
+// If box determination is less than 2, continue comparing as long as there are letters
+
+
